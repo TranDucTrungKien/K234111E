aaed5119ca373650fcd0d9cfd88c05b91bbb6342
kien0 <kientdt234111e@st.uel.edu.vn>
2026-01-18 22:05:50 +0700

    ex14

---
+++ src/app/listcustomer/listcustomer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listcustomer',
+  standalone: false,
+  templateUrl: './listcustomer.html',
+  styleUrl: './listcustomer.css',
+})
+export class Listcustomer {
+
+}
